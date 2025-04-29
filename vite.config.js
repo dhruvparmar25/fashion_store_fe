45402596d08f6674f253106afc7e002753fcaa1b
dhruvparmar25 <parmardhruv034@hmail.com>
2025-04-29 17:52:39 +0530

    main.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fashion_store_fe/",
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
