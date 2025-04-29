4f7bde56db3a4678212b98232a2f7599ba7f4ec5
dhruvparmar25 <parmardhruv034@hmail.com>
2025-04-29 17:55:00 +0530

    main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import router from "./router/router";
 import Vue3Toastify from "vue3-toastify"; // Importing Vue3Toastify
 import "vue3-toastify/dist/index.css"; // Importing CSS for Toastify
 import "./global.css";
+import { createApp } from "vue";
 const app = createApp(App);
 app.use(Vue3Toastify, {
   autoClose: 3000,
